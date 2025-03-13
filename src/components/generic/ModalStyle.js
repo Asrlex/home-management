5e07bfea8a82de2fc0f4b9a646a949366e9967a6
Asrlex <alexsruc@hotmail.com>
2025-03-13 08:26:13 +0100

    move modal styles

---
+++ src/components/generic/ModalStyle.js
@@ -0,0 +1,40 @@
+export const customStyles = {
+  control: (provided) => ({
+    ...provided,
+    backgroundColor: "var(--modal-input-bg-color)",
+    borderColor: "var(--border-color)",
+    color: "var(--text-color)",
+    boxShadow: "none",
+    "&:hover": { borderColor: "var(--border-color)" },
+    maxHeight: "40px",
+  }),
+  input: (provided) => ({ ...provided, color: "var(--text-color)" }),
+  placeholder: (provided) => ({
+    ...provided,
+    color: "var(--context-no-content-bg-color)",
+  }),
+  singleValue: (provided) => ({
+    ...provided,
+    color: "var(--text-color)",
+  }),
+  menu: (provided) => ({
+    ...provided,
+    zIndex: 10500,
+    maxHeight: "350px",
+    overflowY: "auto",
+    backgroundColor: "var(--modal-button-bg-color)",
+    color: "var(--modal-button-text-color)",
+  }),
+  option: (provided, state) => ({
+    ...provided,
+    backgroundColor: state.isSelected
+      ? "var(--modal-button-bg-color)"
+      : state.isFocused
+      ? "var(--modal-button-hover-bg-color)"
+      : "var(--modal-button-bg-color)",
+    color: "var(--modal-button-text-color)",
+    "&:hover": {
+      backgroundColor: "var(--modal-button-hover-bg-color)",
+    },
+  }),
+};
